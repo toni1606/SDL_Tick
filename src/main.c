18561a27ac8a68bd46af35e17834d28427b95381
Toni Guli <toniguli16@gmail.com>
2023-02-18 11:38:24 +0100

    tested turns in main()

--- src/main.c
+++ src/main.c
@@ -4,7 +4,14 @@
 int main(void) {
   game_board_t board;
 
-  game_board_new(&board, 9, 3);
+  game_board_new(&board, 9, 3, O, X);
   game_board_print(&board);
+  for (int i = 0; i < board.len / board.row_len; i++) {
+    for (int j = 0; j < board.row_len; j++) {
+      game_board_turn(&board, i, j);
+      game_board_print(&board);
+      printf("\n");
+    }
+  }
   return 0;
 }
